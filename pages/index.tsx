590a990e0b35e42ae9e58000451ae60d490dde6c
annon-codes <pvt.max@tutanota.com>
2021-03-22 14:21:56 -0400

    styling added for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 import { Chart } from "../interfaces";
 import getAllCharts from "../utils/getCharts";
@@ -8,17 +9,15 @@ interface Props {
 
 export default function Home({ charts }: Props) {
     return (
-        <Layout title={"Look at these charts!"}>
-            <article>
-                <ul>
-                    {charts?.map(({ title, image, description }, idx) => (
-                        <li key={idx}>
-                            <p>{title}</p>
-                            <img src={image} alt={description} />
-                        </li>
-                    ))}
-                </ul>
-            </article>
+        <Layout title={"Look at these charts!"} className="container p-6 mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                {charts.map(({ title, image, description }, idx) => (
+                    <div key={idx}>
+                        <p className="capitalize w-80 md:w-52 truncate text-2xl">{title}</p>
+                        <Image src={"/" + image} alt={description} layout="intrinsic" width={480} height={270} />
+                    </div>
+                ))}
+            </div>
         </Layout>
     )
 }
