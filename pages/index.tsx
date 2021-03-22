407c9c4f4b7e8f73ef22798a741091ec03a86eeb
annon-codes <pvt.max@tutanota.com>
2021-03-22 17:33:22 -0400

    can now click on a chart to see its full page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Layout from "../components/Layout";
 import { Chart } from "../interfaces";
 import getAllCharts from "../utils/getCharts";
@@ -11,10 +12,10 @@ export default function Home({ charts }: Props) {
         <Layout title={"Look at these charts!"} className="container p-6 mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {charts.map(({ title, image, description }, idx) => (
-                    <div key={idx}>
+                    <a key={idx} href={title.split(" ").join("-")}>
                         <p className="capitalize w-80 md:w-52 truncate text-2xl">{title}</p>
                         <img src={image} alt={description} />
-                    </div>
+                    </a>
                 ))}
             </div>
         </Layout>
