e42576298fdce3281e3089d0ea4241dde3b5e220
annon-codes <pvt.max@tutanota.com>
2021-03-22 21:10:15 -0400

    fix syntax issue

--- pages/api/love.ts
+++ pages/api/love.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import s from "../../test.json"
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export default async (_req: NextApiRequest, res: NextApiResponse) => {
     res.json(s);
 }
